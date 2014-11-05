d5b5335cc7c5ad25291f0b207f823ea83cdbc876
Roma <Bob@roma.local>
2014-11-05 15:32:42 +0200

    Initial Commit

---
+++ ComicsFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ComicsFinder
+//
+//  Created by Roma on 11/5/14.
+//  Copyright (c) 2014 Roma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
